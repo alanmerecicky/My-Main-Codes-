2bd90bda353992093dab4b2a7dcf2f5d9a08167c
alanmerecicky <31898499+alanmerecicky@users.noreply.github.com>
2021-01-05 23:42:08 -0500

    MasterMind Application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterMindApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
